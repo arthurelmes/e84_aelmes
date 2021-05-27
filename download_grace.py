687173f520d7c1efefbafd74992a6adbd684fa0f
Arthur Elmes <arthur.elmes@gmail.com>
2021-05-27 16:43:56 -0400

    Fixed part of the dl script, fixed the formatting of the visualizer, misc other edits.

--- download_grace.py
+++ download_grace.py
@@ -6,6 +6,7 @@ import requests
 import os
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
+import glob
 
 import sys
 
@@ -28,14 +29,22 @@ def dl_data(date_start, date_end):
         date_next = date_start + relativedelta(months=1) - relativedelta(days=1)
         date_next_doy = convert_date(date_next)
 
-        base_url = 'https://podaac-tools.jpl.nasa.gov/drive/files/allData/tellus/L3/gracefo/land_mass/RL06/v03/JPL/'
+        #base_url = 'https://podaac-tools.jpl.nasa.gov/drive/files/allData/tellus/L3/grace/land_mass/RL06/v03/JPL'
+        base_url = 'https://podaac-tools.jpl.nasa.gov/drive/files/allData/tellus/L3/grace/land_mass/RL06/v03/JPL/'
         file_front = 'GRD-3_'
         file_date = '{x}-{y}'.format(x=date_start_doy, y=date_next_doy)
-        file_end = '_GRFO_JPLEM_BA01_0600_LND_v03.tif'
+        file_end = '_GRAC_JPLEM_BA01_0600_LND_v03.tif'
 
         dl_url = base_url + file_front + file_date + file_end
-
+        print(dl_url)
         response = requests.get(dl_url, stream=True, auth=('arthur.elmes', 'Yrkob5xXqc@CRW5TJn3'))
+
+        # try:
+        #     response = requests.get(dl_url, stream=True, auth=('arthur.elmes', 'Yrkob5xXqc@CRW5TJn3'))
+        #     response.raise_for_status()
+        # except requests.exceptions.HTTPError as err:
+        #     continue #raise SystemExit(err)
+
 
         test_file_name = file_front + file_date + file_end
 
@@ -49,14 +58,22 @@ def dl_data(date_start, date_end):
         date_start = date_start + relativedelta(months=1)
 
 
+def cleanup(dir):
+    for file in glob.glob(os.path.join(dir, "*.tif")):
+        if os.path.getsize(file) < 350:
+            os.remove(file)
+
 
 if __name__ == '__main__':
     workspace = '/home/arthur/Dropbox/career/e84/sample_data/'
     os.chdir(workspace)
 
     # TODO when users enter year-mm, add the day 1 of that month to construct complete date
-    date_0 = datetime.strptime('2018-06-01', '%Y-%m-%d')
-    date_1 = datetime.strptime('2018-12-01', '%Y-%m-%d')
+    date_0 = datetime.strptime('2003-01-01', '%Y-%m-%d')
+    date_1 = datetime.strptime('2021-12-01', '%Y-%m-%d')
 
     dl_data(date_0, date_1)
+
+    # TODO this needs a real solution!
+    cleanup(workspace)
 
--- viz_grace.py
+++ viz_grace.py
@@ -55,10 +55,10 @@ def do_plot(img_file, o_dir, contrast_stretch, ul_coord, lr_coord):
     # ymin = gt[3] + (yres * ds.RasterYSize)+ yres * 0.5
     # ymax = gt[3] - yres * 0.5
 
-    xmin = ul_coord[1] # -110
-    xmax = lr_coord[1] #
-    ymax = ul_coord[0] #
-    ymin = lr_coord[0] #
+    xmin = ul_coord[1]
+    xmax = lr_coord[1]
+    ymax = ul_coord[0]
+    ymin = lr_coord[0]
 
     # for debugging
     # xmin = -160
@@ -110,21 +110,21 @@ def do_plot(img_file, o_dir, contrast_stretch, ul_coord, lr_coord):
 
     # plotting stuff
     fig = plt.figure(figsize=(12, 6))
-    ax = plt.Axes(fig, [0, 0, 1, 1])
+    title = 'GRACE Land Water-Equivalent-Thickness Surface Mass Anomaly Rel 6.0 v 03 for dates: ' + img_file.split('/')[-1][6:21]
+    plt.title(title, loc='center', pad=22)
 
-    im = m.pcolormesh(xx, yy, data, cmap=plt.cm.coolwarm, shading='auto')
+    im = m.pcolormesh(xx, yy, data, cmap=plt.cm.plasma, shading='auto', vmin=-0.4, vmax=0.4)
 
     # annotate
     m.drawcountries()
     m.drawcoastlines(linewidth=.5)
-    #m.drawmapboundary(fill_color='aqua')
     parallels = np.arange(0., 81, 10.)
     m.drawparallels(parallels, labels=[False, True, True, False])
     meridians = np.arange(10., 351., 20.)
     m.drawmeridians(meridians, labels=[True, False, False, True])
     m.colorbar(im, location='bottom')
+    #plt.clim(-2.0, 0.5)
 
-    plt.show()
     fig.savefig('{a}{b}_{c}_{d}.png'.format(a=o_dir + '/',
                                             b=os.path.basename(img_file[:-4]),
                                             c=str(ul_coord[0]) + 'Deg_' + str(ul_coord[1]) + 'Deg_by',
@@ -132,21 +132,21 @@ def do_plot(img_file, o_dir, contrast_stretch, ul_coord, lr_coord):
     ds = None
 
 
-workspace = '/home/arthur/Dropbox/career/e84/sample_data/'
-out_dir = os.path.join(workspace, 'out')
-os.chdir(workspace)
-#img_0 = 'GRD-3_2018152-2018181_GRFO_JPLEM_BA01_0600_LND_v03.tif'
+if __name__ == '__main__':
+    workspace = '/home/arthur/Dropbox/career/e84/sample_data/'
+    out_dir = os.path.join(workspace, 'png')
+    os.chdir(workspace)
+    #img_0 = 'GRD-3_2018152-2018181_GRFO_JPLEM_BA01_0600_LND_v03.tif'
 
-# make output dir if it doesn't exist
-if not os.path.isdir(out_dir):
-    os.mkdir(out_dir)
+    # make output dir if it doesn't exist
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
 
-# this will be user-entered via cmd
-ul = (70, -130)
-lr = (10, -20)
+    # this will be user-entered via cmd
+    ul = (70, -130)
+    lr = (10, -20)
 
-# loop over all matching input files in workspace, create plot for each
-
-for file in glob.glob(os.path.join(workspace, "*GRFO_JPLEM_BA01_0600_LND_v03.tif")):
-    print(file)
-    do_plot(file, out_dir, True, ul, lr)
+    # loop over all matching input files in workspace, create plot for each
+    for file in glob.glob(os.path.join(workspace, "*GRAC_JPLEM_BA01_0600_LND_v03.tif")):
+        print(file)
+        do_plot(file, out_dir, True, ul, lr)
